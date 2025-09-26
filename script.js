6ec2a88dfabc2ab8081881c3c9285b7447f53b74
Talles <tgtallles362@gmail.com>
2025-09-26 17:29:55 -0300

    comit

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Seleciona os elementos
 const menuToggle = document.getElementById("menu-toggle");
 const menuNav = document.getElementById("menu-nav");
 
-// Alterna o menu ao clicar
 menuToggle.addEventListener("click", () => {
   menuNav.classList.toggle("active");
 });
 
-// Fecha o menu ao clicar em qualquer link
 document.querySelectorAll(".menu-link").forEach(link => {
   link.addEventListener("click", () => {
     menuNav.classList.remove("active");
